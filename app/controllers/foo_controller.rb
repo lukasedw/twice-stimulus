2420fd1a144aecb163d800780c08ad2816dc96fb
Lucas Eduardo <lukaszonecell@gmail.com>
2021-06-14 16:23:23 -0300

    First commit

---
+++ app/controllers/foo_controller.rb
@@ -0,0 +1,7 @@
+class FooController < ApplicationController
+  def index_erb
+  end
+
+  def index_haml
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'foo/index_erb'
+  get 'foo/index_haml'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
